45286c8bf1c7ea545b29021698948ad4c7e9c8c1
Karthik Viswanathan <karthik.ksv@gmail.com>
2022-12-26 10:47:07 -0800

    Make timing test less flaky.

--- test/css_test.ts
+++ test/css_test.ts
@@ -352,7 +352,7 @@ test("generateRuleTemplate cache", () => {
   const time3 = process.hrtime.bigint() - start3
 
   // These values are in ns.
-  expect(time1).toBeGreaterThan(700_000)
+  expect(time1).toBeGreaterThan(450_000)
   expect(time2).toBeLessThan(400_000)
   // The LRU cache seems to perform much better on subsequent accesses.
   expect(time3).toBeLessThan(100_000)
